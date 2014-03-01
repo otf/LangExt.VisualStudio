01d3689d597fec552c767fdd5c2d12af0fc4567d
otf <otf@me.com>
2014-03-02 04:52:53 +0900

    add test for it cannot rewrite

--- src/LangExt.VisualStudio.Syntax.Tests/ShadowingRewriterTest.cs
+++ src/LangExt.VisualStudio.Syntax.Tests/ShadowingRewriterTest.cs
@@ -20,7 +20,6 @@ namespace LangExt.VisualStudio.Syntax.Tests
             Assert.That(rewrited.ChildNodes().Single().ChildNodes().Single(node => node is TNode).ToString(), Is.EqualTo(expectedRewrited));
         }
 
-        [TestCase(@"int x=0;", @"int x=0;")]
         [TestCase(@"int x=0; char x='a';", @"int x=0; char x2='a';")]
         public void RewriteMethod(string snippet, string expected)
         {
@@ -30,7 +29,6 @@ namespace LangExt.VisualStudio.Syntax.Tests
             AssertRewrite<MethodDeclarationSyntax>(fullSnippet, fullExpected);
         }
 
-        [TestCase(@"int x=0;", @"int x=0;")]
         [TestCase(@"int x=0; char x='a';", @"int x=0; char x2='a';")]
         public void RewriteProperty(string snippet, string expected)
         {
@@ -38,6 +36,16 @@ namespace LangExt.VisualStudio.Syntax.Tests
             var fullSnippet = string.Format(methodDeclFormat, snippet);
             var fullExpected = string.Format(methodDeclFormat, expected);
             AssertRewrite<PropertyDeclarationSyntax>(fullSnippet, fullExpected);
+        }
+
+        [TestCase(@"int x=0;", @"int x=0;")]
+        [TestCase(@"int x=0; x=100;", @"int x=0; x=100;")]
+        public void ItCantRewriteProperty(string snippet, string expected)
+        {
+            var methodDeclFormat = "void f(){{ {0} }}";
+            var fullSnippet = string.Format(methodDeclFormat, snippet);
+            var fullExpected = string.Format(methodDeclFormat, expected);
+            AssertRewrite<MethodDeclarationSyntax>(fullSnippet, fullExpected);
         }
     }
 }
