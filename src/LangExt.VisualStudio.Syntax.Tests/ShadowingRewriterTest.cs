72d00fd5e7cbe10389a4635f15a51ee3593bb18a
otf <otf@me.com>
2014-03-02 04:47:36 +0900

    add support for shadowing.

---
+++ src/LangExt.VisualStudio.Syntax.Tests/ShadowingRewriterTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Roslyn.Compilers.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LangExt.VisualStudio.Syntax.Tests
+{
+    [TestFixture]
+    public class ShadowingRewriterTest
+    {
+        void AssertRewrite<TNode>(string snippet, string expectedRewrited)
+        {
+            var source = string.Format(@"class Program {{ {0} }}", snippet);
+            var syntaxTree = SyntaxTree.ParseText(source);
+            var rootNode = syntaxTree.GetRoot();
+            var rewrited = new ShadowingRewriter().Visit(rootNode);
+
+            Assert.That(rewrited.ChildNodes().Single().ChildNodes().Single(node => node is TNode).ToString(), Is.EqualTo(expectedRewrited));
+        }
+
+        [TestCase(@"int x=0;", @"int x=0;")]
+        [TestCase(@"int x=0; char x='a';", @"int x=0; char x2='a';")]
+        public void RewriteMethod(string snippet, string expected)
+        {
+            var methodDeclFormat = "void f(){{ {0} }}";
+            var fullSnippet = string.Format(methodDeclFormat, snippet);
+            var fullExpected = string.Format(methodDeclFormat, expected);
+            AssertRewrite<MethodDeclarationSyntax>(fullSnippet, fullExpected);
+        }
+
+        [TestCase(@"int x=0;", @"int x=0;")]
+        [TestCase(@"int x=0; char x='a';", @"int x=0; char x2='a';")]
+        public void RewriteProperty(string snippet, string expected)
+        {
+            var methodDeclFormat = "int Prop {{  get{{ {0} return 0; }} }}";
+            var fullSnippet = string.Format(methodDeclFormat, snippet);
+            var fullExpected = string.Format(methodDeclFormat, expected);
+            AssertRewrite<PropertyDeclarationSyntax>(fullSnippet, fullExpected);
+        }
+    }
+}
---
+++ src/LangExt.VisualStudio.Syntax/ShadowingRewriter.cs
@@ -0,0 +1,57 @@
+﻿using Roslyn.Compilers.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSharpSyntax = Roslyn.Compilers.CSharp.Syntax;
+
+namespace LangExt.VisualStudio.Syntax
+{
+    public class ShadowingRewriter : SyntaxRewriter
+    {
+        readonly Dictionary<string, int> identities = new Dictionary<string, int>();
+
+        public ShadowingRewriter()
+            : base()
+        {
+        }
+
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            identities.Clear();
+            return base.VisitMethodDeclaration(node);
+        }
+
+        public override SyntaxNode VisitAccessorDeclaration(AccessorDeclarationSyntax node)
+        {
+            identities.Clear();
+            return base.VisitAccessorDeclaration(node);
+        }
+
+        public override SyntaxNode VisitIdentifierName(IdentifierNameSyntax node)
+        {
+            if (identities.ContainsKey(node.Identifier.ValueText) && 1 < identities[node.Identifier.ValueText])
+                return base.VisitIdentifierName(node.WithIdentifier(CSharpSyntax.Identifier(node.Identifier.ValueText + identities[node.Identifier.ValueText])));
+            else
+                return base.VisitIdentifierName(node);
+        }
+
+        public override SyntaxNode VisitVariableDeclarator(VariableDeclaratorSyntax node)
+        {
+            if (node.Ancestors().Any(anc => anc is FieldDeclarationSyntax))
+                return base.VisitVariableDeclarator(node); ;
+
+            if (identities.ContainsKey(node.Identifier.ValueText))
+            {
+                ++identities[node.Identifier.ValueText];
+                return base.VisitVariableDeclarator(node.WithIdentifier(CSharpSyntax.Identifier(node.Identifier.ValueText + identities[node.Identifier.ValueText])));
+
+            }
+            else
+            {
+                identities[node.Identifier.ValueText] = 1;
+                return base.VisitVariableDeclarator(node);
+            }
+        }
+    }
+}
